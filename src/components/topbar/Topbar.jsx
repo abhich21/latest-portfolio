835ecf086670e65352d41113e25949f3dff96e96
abhich21 <96258754+abhich21@users.noreply.github.com>
2022-05-17 23:20:29 +0530

    updated cv link

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -37,7 +37,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                     </div>
                     <div className="itemContainer1" id="lastDiv">
                         <a
-                            href="https://drive.google.com/file/d/1F7UbW6eQwtiXeESwl6LYJcgCSnG7y142/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1pSLliT9lLQ96gTCCV1eOfGUYl_lsUsE3/view?usp=sharing"
                             target="blank"
                             id="last"
                         >
